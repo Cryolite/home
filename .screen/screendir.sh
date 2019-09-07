244661ae4b929131920e13930ec08843c5a7ddbc
Cryolite <cryolite.indigo@gmail.com>
2019-09-08 03:05:31 +0900

    Add support for `screen -ls` ending with `\r\n`
    On some versions of `screen` (confirmed on 4.06.02 at least), the stdout
    of `screen -ls` ends with `\r\n`, not `\n`. If this carriage return in
    the stdout remains, it becomes a trouble when characters are appended to
    the stdout. This commit fixes it.

--- .screen/screendir.sh
+++ .screen/screendir.sh
@@ -5,6 +5,7 @@ set -euo pipefail
 . "$HOME/.local/bin/common.sh"
 
 # `screen -ls` on screen 4.0 exits with a non-zero status code.
+# `screen -ls` on screen 4.06.02 ends with `\r\n`, not `\n`.
 { screen -ls || true; } \
   | grep -E '^(No Sockets found|1 Socket|[1-9][[:digit:]]* Sockets) in ' \
-  | sed -e 's/^\(No Sockets found\|1 Socket\|[[:digit:]]* Sockets\) in \(.*\)\./\2/'
+  | sed -e 's/^\(No Sockets found\|1 Socket\|[[:digit:]]* Sockets\) in \(.*\)\.[[:space:]]*$/\2/'
